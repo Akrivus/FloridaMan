11866757f29f8c1037def9765422a909d31de37e
Owen Adrian <akrivus@gmail.com>
2019-10-08 13:09:04 -0500

    Updated regex and username.

--- app.rb
+++ app.rb
@@ -10,8 +10,8 @@ def cache!
   end
 end
 
-REDDIT = Redd.it(user_agent: 'Redd:FloridaMan:19.8.10 (by /u/akrivus)', client_id: ENV['REDDIT_CLIENTID'], secret: ENV['REDDIT_SECRETID'], username: ENV['REDDIT_USERNAME'], password: ENV['REDDIT_PASSWORD'])
-FLORIDA_MAN_REGEX = /^florida (?:wo){0,1}man/i
+REDDIT = Redd.it(user_agent: "Redd:FloridaMan:19.8.10 (by /u/#{ENV['REDDIT_USERNAME']})", client_id: ENV['REDDIT_CLIENTID'], secret: ENV['REDDIT_SECRETID'], username: ENV['REDDIT_USERNAME'], password: ENV['REDDIT_PASSWORD'])
+FLORIDA_MAN_REGEX = /^florida man/i
 Thread.new(REDDIT) do |session|
   session.subreddit('FloridaMan').post_stream.each do |post|
     next if not post.title.match? FLORIDA_MAN_REGEX
