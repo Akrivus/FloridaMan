6c8b4a5ebd54aec3fbbb27433f1036ec143f4fcb
Owen Adrian <akrivus@gmail.com>
2019-10-08 12:55:15 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Florida Man
+
+Mention me to get the latest news on the local Florida Man from Florida.
+
+## How It Works
+
+* Florida Man opens a thread that polls reddit posts from /u/FloridaMan starting with 'Florida Man.'
+* Florida Man reformats these posts and pushes them to a mutex'd array.
+* Florida Man's main thread houses a Discord bot that samples from the array when mentioned.
---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'dotenv/load'
+require 'discordrb'
+require 'redd'
+
+CACHE_SEMAPHORE = Mutex.new
+CACHE = []
+def cache!
+  CACHE_SEMAPHORE.synchronize do
+    CACHE.sample
+  end
+end
+
+REDDIT = Redd.it(user_agent: 'Redd:FloridaMan:19.8.10 (by /u/akrivus)', client_id: ENV['REDDIT_CLIENTID'], secret: ENV['REDDIT_SECRETID'], username: ENV['REDDIT_USERNAME'], password: ENV['REDDIT_PASSWORD'])
+FLORIDA_MAN_REGEX = /^florida (?:wo){0,1}man/i
+Thread.new(REDDIT) do |session|
+  session.subreddit('FloridaMan').post_stream.each do |post|
+    next if not post.title.match? FLORIDA_MAN_REGEX
+    CACHE_SEMAPHORE.synchronize do
+      CACHE.push(post.title.gsub(FLORIDA_MAN_REGEX, '').strip)
+    end
+  end
+end
+
+BOT = Discordrb::Bot.new(token: ENV['DISCORD_TOKEN'])
+BOT.mention do |event|
+  event.respond("_#{cache!}_")
+  event.message.delete
+end
+BOT.run
